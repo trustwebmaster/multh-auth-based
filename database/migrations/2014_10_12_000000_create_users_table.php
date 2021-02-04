7fe4f2f9a4e426902f0dc21664a4c718e0ba1a78
@dev_cs <tmusikiri43@gmail.com>
2021-02-04 03:40:12 +0200

    created user roles and fields

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('phone');
+            $table->string('state');
+            $table->integer('role');
+            $table->integer('status_id')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
